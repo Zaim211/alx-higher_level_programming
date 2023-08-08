c597bbe644ec25c7db2ffc098f09f6fb33de3c5a
Zaim211 <yousseftitzaim@gmail.com>
2023-08-08 07:28:52 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range (100):
-    if (n != 99)
+    if (n != 99):
         print("{}{}, ".format(int(n / 10), n % 10), end="")
-    else
+    else:
         print("{}{}, ".format(int(n / 10), n % 10))
