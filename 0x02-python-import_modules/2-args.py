e0d3a508c87dfa8d3ab35beba32bdc8f1e684d92
Zaim211 <yousseftitzaim@gmail.com>
2023-08-10 06:28:13 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    x = length(sys.argv) - 1
+
+    if x == 0:
+        print("{} argument".format(x))
+    elif x == 1:
+        print("{} argument".format(x))
+    else:
+        print("{} argument".format(x))
+
+    if x >= 1:
+        x = 0
+        for arg in sys.argv:
+            if x != 0:
+                print("{}: {}".format(x, arg))
+            x += 1
