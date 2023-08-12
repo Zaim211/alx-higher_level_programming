74004c583ec2f80dafd28b97d7e7c4d8c30de5dc
Zaim211 <yousseftitzaim@gmail.com>
2023-08-12 10:39:09 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,8 @@ def multiple_returns(sentence):
     len_sent = len(sentence)
 
     if len_sent == 0:
-        new_tuple = len(lent_sent, None)
+        new_tuple = len(len_sent, None)
     else:
-        new_tuple = len(lent_sent, sentence[0])
+        new_tuple = len(len_sent, sentence[0])
 
     return (new_tuple)
