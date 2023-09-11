cf335e78ca14e88f22326d9e153f07cdce7b079c
Zaim211 <yousseftitzaim@gmail.com>
2023-09-11 11:51:50 -0700

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
+class MyList(obj):
+    """subclass of list"""
+    def print_sorted(self):
+         """initializes the object"""
+         super().__init__()
+
+    def print_sorted(self):
+        """print the sorted list"""
+        print(sorted(list))
