14e6921ed4308dcbd6b89447209830f59dc90454
Zaim211 <yousseftitzaim@gmail.com>
2023-09-12 13:25:34 -0700

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Contains a Class Student"""
+"""Defines a Class Student"""
 
 
 class Student:
     """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
         Args:
