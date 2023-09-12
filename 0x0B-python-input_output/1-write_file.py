e615b808742dc6e6d4df58d4e42ee2c691f7d41b
Zaim211 <yousseftitzaim@gmail.com>
2023-09-12 11:51:06 -0700

    Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/sur/bin/python3
+"""
+Contains the write_file function
+"""
+
+
+def write_file(filename="", text=""):
+    """returns the number of chars written to "filename" from "text"""
+    with open(filename, 'w', encouding="utf-8") as f:
+        return f.write(text)
