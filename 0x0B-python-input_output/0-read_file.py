fb484d665b78134719b999be205fa81c543e2b7e
Zaim211 <yousseftitzaim@gmail.com>
2023-09-12 11:38:27 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ Contains the read_file function
 
 def read_file(filename=""):
     """""reads a text file(UTF8) and prints it to stdout"""
-    with open('workfile', 'r', encoding="utf-8") as f:
+    with open('filename', 'r', encoding="utf-8") as f:
         print(f.read(), end="")
