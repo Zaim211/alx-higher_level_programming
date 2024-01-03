d700520de7c2d3ea6098209553ac807e1e5ffc0a
Zaim211 <yousseftitazaim@gmail.com>
2024-01-03 11:44:13 -0800

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error == null) {
+    const res = {};
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (res[json[i].userId] === undefined) {
+          res[json[i].userId] = 0;
+        }
+        res[json[i].userId]++;
+      }
+    }
+    console.log(res);
+  }
+});
