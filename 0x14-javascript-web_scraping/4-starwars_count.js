51a3c4630172dc76551e589925f4427d58d7078a
Zaim211 <yousseftitazaim@gmail.com>
2024-01-03 11:35:25 -0800

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,11 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const res = JSON.parse(body).res;
-    console.log(res.reduce((count, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
-	? count + 1
-	: count;
+        ? count + 1
+        : count;
     }, 0));
   }
 });
